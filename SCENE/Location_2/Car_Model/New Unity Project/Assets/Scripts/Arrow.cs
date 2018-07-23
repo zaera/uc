35b7738e2cfa8360bf9a21fb5746daf92c9a93de
Morphema <36934883+Morphema@users.noreply.github.com>
2018-07-23 19:12:31 +0300

    Location_2

---
+++ SCENE/Location_2/Car_Model/New Unity Project/Assets/Scripts/Arrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    public float speed = 50f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.down,speed * Time.deltaTime);
+        }
+	}
+}
---
+++ SCENE/Location_2/Car_Model/New Unity Project/Assets/Scripts/SwapCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCamera : MonoBehaviour {
+
+    public GameObject[] cameras;
+    public int activeCamera;
+
+	
+	void Start () {
+        activeCamera = 0;
+        foreach (GameObject cam in cameras)
+        {
+            cam.SetActive(false); //Отключение всех камер
+        }
+        cameras[activeCamera].SetActive(true);
+	}
+	
+
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Swap_Cam();
+        }
+	}
+
+    void Swap_Cam()
+    {
+        cameras[activeCamera].SetActive(false); //Выключение активной камеры
+
+        if (activeCamera + 1 >= cameras.Length)
+        {
+            activeCamera = 0;
+        }
+
+        else {
+            activeCamera++;
+        }
+
+        cameras[activeCamera].SetActive(true);
+    }
+}
